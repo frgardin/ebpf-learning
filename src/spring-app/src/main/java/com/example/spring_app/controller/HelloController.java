80bc4302015ae537a479544a92e2e712e4e14b9b
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-06-05 00:03:39 -0300

    produce first controller

---
+++ src/spring-app/src/main/java/com/example/spring_app/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.spring_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    String hello() {
+        return "HELLOOWW";
+    }
+}
