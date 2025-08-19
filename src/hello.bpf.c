bbc4f4749979e57316111fec09419abde2e5efaf
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-08-18 23:35:21 -0300

    Switches tracepoint to sys_enter_write with PID filter
    Replaces the execve tracepoint with sys_enter_write and introduces
    an optional static PID filter to restrict logging. Enhances log
    messages to include the triggering process ID and updates license
    declaration for broader compatibility.

--- src/hello.bpf.c
+++ src/hello.bpf.c
@@ -1,10 +1,20 @@
+#define BPF_NO_GLOBAL_DATA
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
 
-SEC("tracepoint/syscalls/sys_enter_execve")
-int hello(struct trace_event_raw_sys_enter *ctx) {
-    bpf_printk("Hello World!!\\n");
-    return 0;
+typedef unsigned int u32;
+typedef int pid_t;
+const pid_t pid_filter = 0;
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
+SEC("tp/syscalls/sys_enter_write")
+int handle_tp(void *ctx)
+{
+ pid_t pid = bpf_get_current_pid_tgid() >> 32;
+ if (pid_filter && pid != pid_filter)
+  return 0;
+ bpf_printk("BPF triggered sys_enter_write from PID %d.\n", pid);
+ return 0;
 }
-
-char LICENSE[] SEC("license") = "GPL";
