5aecccaec516088f662ecee7b1f815781fcedbb4
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-09-25 00:44:06 +0000

    create possible ebpfagent for that

--- src/main/java/com/unicamp/ebpfagent/EbpfAgent.java
+++ src/main/java/com/unicamp/ebpfagent/EbpfAgent.java
@@ -1,22 +1,15 @@
 package com.unicamp.ebpfagent;
 
-import java.io.IOException;
 
 public class EbpfAgent {
-    public static void main(String[] args) throws IOException, Exception {
-        int mapFd = LibBpf.INSTANCE.bpfObjGet("/sys/fs/bpf/execCounter");
-        EbpfMapReader reader = new EbpfMapReader(mapFd);
-        PrometheusExporter exporter = new PrometheusExporter();
+
+    public static void main(String[] args) throws Exception {
+        int ringFd = LibBpf.INSTANCE.bpfObjGet("/sys/fs/bpf/output");
+        RingBufferReader ringReader = new RingBufferReader(ringFd, 4096);
 
         while (true) {
-            for (int pid = 0; pid < Integer.MAX_VALUE; pid++) {
-                long count = reader.getCount(pid);
-                if (count > 0) {
-                    Thread.sleep(1000);
-                    System.out.println("pid= " + pid + "count= " +count);
-                    exporter.update(pid, count);
-                }
-            }
+            ringReader.pollAndRead();
+
             Thread.sleep(1000);
         }
     }
