26768bc3ffe0257b32ffbd834212d1ed633609f5
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-08 23:32:17 -0300

    cpp learning

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main() {
 
-    cout << "Hello World!" << endl;
-    return 0;    
+    char v[8] = "abacate";
+    char* p = &v[3];
+    cout << p << endl;
+    
+    char x = *p;
+    cout << x << endl;
+
+    for (auto& c : v) {
+        cout << c << ' ';   
+    }
 }
