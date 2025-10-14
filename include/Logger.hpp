2e4ef8cd974ff3911bd81356c8e08c2a88ed0350
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-14 19:09:52 +0000

    add logger

---
+++ include/Logger.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class Logger {
+public:
+    static void info(const std::string& msg) { std::cout << "[INFO] " << msg << std::endl; }
+    static void error(const std::string& msg) { std::cerr << "[ERROR] " << msg << std::endl; }
+};
