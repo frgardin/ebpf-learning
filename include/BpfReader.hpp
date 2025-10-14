1e29932b937d8d6cadccfac500ba6f0c752153f7
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-14 19:09:29 +0000

    add a bpf map reader

---
+++ include/BpfReader.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <bpf/bpf.h>
+#include <string>
+#include <unordered_map>
+#include <stdexcept>
+
+class BpfReader {
+private:
+    int mapFd;
+    std::string mapPath;
+
+public:
+    explicit BpfReader(const std::string& pinnedPath);
+    std::unordered_map<int, long> readAll();
+};
