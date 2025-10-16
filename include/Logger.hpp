fce5d62267154cca9f5845714d43ccac4bc5e6e4
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-16 17:54:24 +0000

    add debug to logger class

--- include/Logger.hpp
+++ include/Logger.hpp
@@ -6,5 +6,6 @@ class Logger
 {
 public:
     static void info(const std::string &msg) { std::cout << "[INFO] " << msg << std::endl; }
+    static void debug(const std::string &msg) { std::cout << "[DEBUG] " << msg << std::endl; }
     static void error(const std::string &msg) { std::cerr << "[ERROR] " << msg << std::endl; }
 };
