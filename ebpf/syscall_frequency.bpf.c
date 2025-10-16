4b6c084e574d863fe80ed852ebc3b6b8c1662745
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-16 20:26:30 +0000

    extract function

--- ebpf/syscall_frequency.bpf.c
+++ ebpf/syscall_frequency.bpf.c
@@ -8,71 +8,73 @@ struct {
     __uint(max_entries, 8);
 } syscall_counts SEC(".maps");
 
-// File System Operations (extremely frequent)
-SEC("tracepoint/syscalls/sys_enter_read")
-int trace_read(void *ctx) {
-    int key = 0;
+int update_elem(int key) {
+    long init_val=1;
     long *value = bpf_map_lookup_elem(&syscall_counts, &key);
     if (value) __sync_fetch_and_add(value, 1);
+    else {
+        bpf_map_update_elem(&syscall_counts, &key, &init_val, BPF_ANY);
+    }
+    return 0;
+}
+
+// File System Operations (extremely frequent)
+SEC("tracepoint/syscalls/sys_enter_read")
+int trace_sys_enter_read(void *ctx) {
+    int key = 0;
+    update_elem(key);
     return 0;
 }
 
 SEC("tracepoint/syscalls/sys_enter_write")
-int trace_write(void *ctx) {
+int trace_sys_enter_write(void *ctx) {
     int key = 1;
-    long *value = bpf_map_lookup_elem(&syscall_counts, &key);
-    if (value) __sync_fetch_and_add(value, 1);
+    update_elem(key);
     return 0;
 }
 
 SEC("tracepoint/syscalls/sys_enter_openat")
-int trace_openat(void *ctx) {
+int trace_sys_enter_openat(void *ctx) {
     int key = 2;
-    long *value = bpf_map_lookup_elem(&syscall_counts, &key);
-    if (value) __sync_fetch_and_add(value, 1);
+    update_elem(key);
     return 0;
 }
 
 SEC("tracepoint/syscalls/sys_enter_close")
-int trace_close(void *ctx) {
+int trace_sys_enter_close(void *ctx) {
     int key = 3;
-    long *value = bpf_map_lookup_elem(&syscall_counts, &key);
-    if (value) __sync_fetch_and_add(value, 1);
+    update_elem(key);
     return 0;
 }
 
 // Memory Operations (very frequent)
 SEC("tracepoint/syscalls/sys_enter_mmap")
-int trace_mmap(void *ctx) {
+int trace_sys_enter_mmap(void *ctx) {
     int key = 4;
-    long *value = bpf_map_lookup_elem(&syscall_counts, &key);
-    if (value) __sync_fetch_and_add(value, 1);
+    update_elem(key);
     return 0;
 }
 
 SEC("tracepoint/syscalls/sys_enter_brk")
-int trace_brk(void *ctx) {
+int trace_sys_enter_brk(void *ctx) {
     int key = 5;
-    long *value = bpf_map_lookup_elem(&syscall_counts, &key);
-    if (value) __sync_fetch_and_add(value, 1);
+    update_elem(key);
     return 0;
 }
 
 // Process Operations
 SEC("tracepoint/syscalls/sys_enter_getpid")
-int trace_getpid(void *ctx) {
+int trace_sys_enter_getpid(void *ctx) {
     int key = 6;
-    long *value = bpf_map_lookup_elem(&syscall_counts, &key);
-    if (value) __sync_fetch_and_add(value, 1);
+    update_elem(key);
     return 0;
 }
 
 // Time Operations
 SEC("tracepoint/syscalls/sys_enter_clock_gettime")
-int trace_clock_gettime(void *ctx) {
+int trace_sys_enter_clock_gettime(void *ctx) {
     int key = 7;
-    long *value = bpf_map_lookup_elem(&syscall_counts, &key);
-    if (value) __sync_fetch_and_add(value, 1);
+    update_elem(key);
     return 0;
 }
 
