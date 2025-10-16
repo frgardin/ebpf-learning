0bf8ca39bd5c3604bd5ed9d5ec9c38b70eea0f6c
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-16 22:55:33 +0000

    testing influx

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,20 @@
 #include <chrono>
 #include "BpfSyscallFrequencyReader.hpp"
 #include "Logger.hpp"
+#include "InfluxClient.hpp"
 
 int main() {
 
     Logger::setLogLevel(LogLevel::INFO);
+
+    InfluxClient influxClient("http", "localhost", 8086, "syscall_metrics");
+
+    if (influxClient.ping()) {
+        Logger::info("Successfully connected to InfluxDB");
+    } else {
+        Logger::error("Failed to connect to InfluxDB");
+        return 1;
+    }
     
     try {
         BpfSyscallFrequencyReader bpfReader;
@@ -29,6 +39,19 @@ int main() {
                     bpfReader.getSyscallName(key), 
                     std::to_string(count)
                 );
+            }
+
+            // Write to InfluxDB
+            std::vector<std::string> lines;
+            for (const auto& [key, count] : data) {
+                std::string syscall_name = bpfReader.getSyscallName(key);
+                std::string line = std::format("syscall_counts,syscall={} count={}", syscall_name, count);
+                lines.push_back(line);
+            }
+            if (influxClient.writeBatch(lines)) {
+                Logger::info("Successfully wrote syscall counts to InfluxDB");
+            } else {
+                Logger::error("Failed to write syscall counts to InfluxDB");
             }
             
             std::this_thread::sleep_for(std::chrono::seconds(2));
