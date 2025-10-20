85375ad97f77de7e7b97bb05644ede547ec224fd
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-20 20:50:17 +0000

    reduce wait time

--- src/RingBufReader.cpp
+++ src/RingBufReader.cpp
@@ -99,6 +99,6 @@ void RingBufReader::read_loop() {
             }
         }
         
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 }
