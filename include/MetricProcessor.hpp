5cbf69a0a582c3a3e69a32fc723bf4a9a09ae3a4
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-21 17:12:09 +0000

    remove unused classes

--- include/MetricProcessor.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <unordered_map>
-#include <string>
-
-class MetricProcessor
-{
-public:
-    static std::unordered_map<std::string, double> process(const std::unordered_map<int, long> &raw);
-};
